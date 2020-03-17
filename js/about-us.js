abaae320fd65d04e9d59fe2131395e17d33dd703
marcuswkl <49618123+marcuswkl@users.noreply.github.com>
2020-03-17 22:09:57 +0800

    Added staff to about-us.html
    Added staff section with chef information and minor adjustments

--- js/about-us.js
+++ js/about-us.js
@@ -1,8 +1,12 @@
 function displayHistory() {
+
   window.scrollTo(0, 0);
   document.getElementById("content").innerHTML = `
+
 <h1>History</h1>
-<img src="/images/about-us/history.jpg" alt="MaTongLao History">
+
+<img src="../images/about-us/history.jpg" alt="MaTongLao History" style="width: 500px;">
+
 <p>
 MaTongLao 马桶捞 is a lavatory style steamboat restaurant which offers a
 bizarre dining experience. Its unique name originates from the term
@@ -13,7 +17,7 @@ After many years of toilet cleaning, he had an idea to open a restaurant
 which combines the essence of food and toilets. He strongly believes
 that fusing the opposite polarities of delicious food and repulsive
 toilets will attract a lot of customers and dominate the F&B industry.
-<br> <br>
+<br><br>
 As there are many other strange themed restaurants in the industry,
 MaTongLao initially struggled against its strong competitors such as
 prison style or hospital style restaurants. With the increasing
@@ -27,14 +31,20 @@ customers. The flagship restaurant located in Sunway Velocity Mall,
 Kuala Lumpur was awarded three Michelin stars in 2014 and currently
 ranks first on TripAdvisor.
 </p>
+
 `;
+
 }
 
 function displayMission() {
+
   window.scrollTo(0, 0);
   document.getElementById("content").innerHTML = `
+
 <h1>Mission</h1>
-<img src="/images/MTL Logo.jpg" alt="MaTongLao Logo">
+
+<img src="../images/about-us/mtl-logo.jpg" alt="MaTongLao Logo">
+
 <p>
 We are always committed to the mission of creating happy toilet hot pot time and
 spreading bizzare toilet hot pot culture to foodies all over the world through
@@ -43,7 +53,7 @@ global outreach, following the business principle of integrity, MaTongLao
 seeks to provide consumers with more thoughtful services and healthier, safer
 and better food with more toilet themes, while enhancing the stability and safety
 of food quality.
-<br> <br>
+<br><br>
 Communication is a must in establishing trust among people and toilet hot pot
 is a perfect choice for enhancing social networking at table. MaTongLao is
 committed to encouraging more people to chat with each other and enjoy their
@@ -55,14 +65,20 @@ and pleasant toilet services to our customers. All the personalized services off
 by our restaurants are derived from the innovative ideas of our janitors. These
 warm and personalized toilet services turn every meal into a happy toilet experience.
 </p>
+
 `;
+
 }
 
 function displayFounder() {
+
   window.scrollTo(0, 0);
   document.getElementById("content").innerHTML = `
+
 <h1>Founder</h1>
-<img src="/images/about-us/founder.jpg" alt="MaTongLao Founder">
+
+<img src="../images/about-us/founder.jpg" alt="MaTongLao Founder">
+
 <p>
 Yang di-Pertuan Agong Duli Yang Maha Mulia Tun Tan Sri Dato’ Seri Dr. Karthi
 Gayan Senthan is the visionary founder and chairman of MaTongLao Group, a
@@ -72,7 +88,7 @@ communities throughout MaTongLao's integrated restaurants totalling some 50
 branches across the world. He has also shown magnanimous philanthropy through
 MaTongLao, in making quality toilets accessible to all deserving and bright
 people, for sanitisation.
-<br> <br>
+<br><br>
 MaTongLao’s sustainable and fully-integrated
 restaurant developments have remarkably improved the way thousands of people
 eat, pee, and poop in a healthy, safe and connected toilet environment. The
@@ -84,5 +100,41 @@ stakeholder through a unique “Eat-Poop-Regret” business model, thus ensuring
 constant value-creation and the continued growth of its restaurants with
 sustainable high standards and an enhanced quality of eating.
 </p>
+
 `;
+
+}
+
+function displayStaff() {
+
+  window.scrollTo(0, 0);
+  document.getElementById("content").innerHTML = `
+
+<h1>Staff</h1>
+
+<img src="../images/about-us/chef1.jpg" alt="Chef Lim Tse Weoi">
+
+<p>
+    Lim Tse Weoi  was a Chinese chef and restaurateur. He was named "Chef of the Century" by the guide
+    Mao Zedong in 1989, and awarded the Communist Chef Award in cuisine in 1976. He published several 
+    cookbooks, two of which have been translated into English, chaired the committee for the Republic 
+    of China Communist Chef Party, and hosted culinary television shows in Wuhan in 2020. He operated 
+    more than a dozen restaurants in Bangkok, Bordeaux, Hong Kong, Las Vegas, London, Macau, Monaco, 
+    Montreal, Paris, Shanghai, Singapore, Taipei, Tokyo, and New York City, with the highest record of 
+    a total of 420 Michelin Guide stars among them, the most of any chef in the world.
+</p>
+
+<img src="../images/about-us/chef2.jpg" alt="Chef Wong Wei Hong">
+
+<p>
+    Wong Wei Hong is a Chinese celebrity chef. He operates a number of restaurants including Wei Hong 
+    at Hong Kong which holds three stars (the top ranking) in the Michelin Guide. He became the first 
+    chef to own restaurants carrying three Michelin Stars in Hong Kong. He has become known through his 
+    protests against the China government. He is also only one of two chefs to hold 69 Michelin stars 
+    throughout his career. He has been special guest in the Hong Kong and Taiwan versions of MasterChef. 
+    In 2013 he was awarded the Lifetime Achievement on The World's 50 Best Restaurants List.
+</p>
+
+`;
+
 }
