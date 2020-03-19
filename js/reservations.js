d4601ff227a7725da533cf642f0f601cfd2602e6
marcuswkl <49618123+marcuswkl@users.noreply.github.com>
2020-03-19 09:02:02 +0800

    Enforced form data integrity in reservations
    Removed submit javascript function in reservations.js and added required to all essential fields in reservations.html

--- js/reservations.js
+++
@@ -1,7 +0,0 @@
-var submitButton = document.querySelector("submit");
-
-function clickSubmit() {
-    alert("Your reservation has been submitted! Please wait for our confirmation email.");
-}
-
-submitButton.addEventListener("click", clickSubmit);
